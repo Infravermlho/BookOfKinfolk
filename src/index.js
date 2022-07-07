c2af9459b5eebf0d8dae53787f2867016268f926
Infraveev <lucassouzasilva74@hotmail.com>
2022-07-07 00:51:02 -0300

    Redoing stuff

--- src/index.js
+++ src/index.js
@@ -1,27 +1,4 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { router} from './router.js'
-import PrimeVue from 'primevue/config'
 import App from './App.vue'
 
-import Button from 'primevue/button';
-import Breadcrumb from 'primevue/breadcrumb';
-import ScrollPanel from 'primevue/scrollpanel';
-import Listbox from 'primevue/listbox';
-
-import "primevue/resources/themes/saga-blue/theme.css"
-import "primevue/resources/primevue.min.css"
-import "primeicons/primeicons.css"    
-
-const pinia = createPinia()
-const app = createApp(App)
-
-app.use(pinia)
-app.use(router)
-app.use(PrimeVue)
-
-app.component('Button', Button)
-app.component('Breadcrumb', Breadcrumb)
-app.component('ScrollPanel', ScrollPanel)
-
-app.mount('#app')
+createApp(App).mount('#app')
--- src/stores/ContentStore.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from "pinia"
-
-export const useContentStore = defineStore("ContentStore", {
-    state: () => {
-        return {
-        }
-    }
-})
-
